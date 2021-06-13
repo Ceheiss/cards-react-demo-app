54330b5633f12c7c3c0f3859bf65ab262b71b66d
Ceheiss <cristobal.heiss.j@gmail.com>
2021-06-13 21:58:32 +0200

    pass data from child to parent component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import "./App.css";
+import { useState } from "react";
 import Card from "./components/Card/Card";
 import cardData from "./data/card-data";
 
 function App() {
+  const [selectedPlace, setSelectedPlace] = useState("");
   return (
-    <div className="cards">
-      {cardData.map((card) => (
-        <Card name={card.name} image={card.image} />
-      ))}
-    </div>
+    <>
+      <h1 className="selected-place">{selectedPlace}</h1>
+      <div className="cards">
+        {cardData.map((card) => (
+          <Card
+            name={card.name}
+            image={card.image}
+            selectPlace={(place) => setSelectedPlace(place)}
+          />
+        ))}
+      </div>
+    </>
   );
 }
 
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./Card.css";
 
-const Card = ({ name, image }) => {
+const Card = ({ name, image, selectPlace }) => {
   return (
     <div className="card">
       <h3>{name}</h3>
       <img src={image} alt="some random place" />
+      <button onClick={() => selectPlace(name)}>Select Place</button>
     </div>
   );
 };
